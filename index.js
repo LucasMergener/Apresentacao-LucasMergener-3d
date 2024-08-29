aa0921163137ad14ab8a57101dab0a14df593e97
LucasMergener <127799242+LucasMergener@users.noreply.github.com>
2024-08-29 01:26:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const primeiroNome = "Lucas";
+const sobrenome = "Mergener";
+const idade = 16;
+const altura = 1.66;
+const cidadeNatal = "São José dos Pinhais";
+const massa = 61;
+
+console.log("O meu primeiro nome é:");
+console.log(primeiroNome);
+console.log("O nome da minha familia é:");
+console.log(sobrenome);
+console.log("A minha idade é:");
+console.log(idade);
+console.log("A minha altura é:");
+console.log(altura);
+console.log("O meu peso é:");
+console.log(massa);
+console.log("Eu nasci em:");
+console.log(cidadeNatal);
